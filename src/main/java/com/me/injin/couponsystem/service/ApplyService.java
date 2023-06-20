06833ebc6520ec35f5add8da6d6a6adc3995eb4b
injinjeong <snowwwwww@naver.com>
2023-06-20 16:00:47 +0900

    redis incr 활용

--- src/main/java/com/me/injin/couponsystem/service/ApplyService.java
+++ src/main/java/com/me/injin/couponsystem/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.me.injin.couponsystem.service;
 
 import com.me.injin.couponsystem.domain.Coupon;
+import com.me.injin.couponsystem.reopsitory.CouponCountRepository;
 import com.me.injin.couponsystem.reopsitory.CouponRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,15 @@ public class ApplyService {
 
     private final CouponRepository couponRepository;
 
-    public ApplyService(CouponRepository couponRepository) {
+    private final CouponCountRepository couponCountRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
         this.couponRepository = couponRepository;
+        this.couponCountRepository = couponCountRepository;
     }
 
     public void apply(Long userId) {
-        long count = couponRepository.count();
+        Long count = couponCountRepository.increment();
 
         if (count > 100) {
             return;
