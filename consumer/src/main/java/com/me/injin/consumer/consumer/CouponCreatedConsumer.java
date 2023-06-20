17e3fa2de3063c2ff09853a631dc5a99f32ff2d7
injinjeong <snowwwwww@naver.com>
2023-06-21 08:54:14 +0900

    failed event 추가 consumer try catch 추가

--- consumer/src/main/java/com/me/injin/consumer/consumer/CouponCreatedConsumer.java
+++ consumer/src/main/java/com/me/injin/consumer/consumer/CouponCreatedConsumer.java
@@ -1,7 +1,11 @@
 package com.me.injin.consumer.consumer;
 
 import com.me.injin.consumer.domain.Coupon;
+import com.me.injin.consumer.domain.FailedEvent;
 import com.me.injin.consumer.repository.CouponRepository;
+import com.me.injin.consumer.repository.FailedEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +14,23 @@ public class CouponCreatedConsumer {
 
     private final CouponRepository couponRepository;
 
-    public CouponCreatedConsumer(CouponRepository couponRepository) {
+    private final FailedEventRepository failedEventRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(CouponCreatedConsumer.class);
+
+    public CouponCreatedConsumer(CouponRepository couponRepository, FailedEventRepository failedEventRepository) {
         this.couponRepository = couponRepository;
+        this.failedEventRepository = failedEventRepository;
     }
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
 //        System.out.println("userId = " + userId);
-        couponRepository.save(new Coupon(userId));
+        try {
+            couponRepository.save(new Coupon(userId));
+        } catch (Exception e) {
+            logger.error("failed to create coupon: " + userId);
+            failedEventRepository.save(new FailedEvent(userId));
+        }
     }
 }
---
+++ consumer/src/main/java/com/me/injin/consumer/repository/FailedEventRepository.java
@@ -0,0 +1,7 @@
+package com.me.injin.consumer.repository;
+
+import com.me.injin.consumer.domain.FailedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FailedEventRepository extends JpaRepository<FailedEvent, Long> {
+}
